dcddd0e9b1ea9ccc27aa4e5d60628202f3c7984b
Kriti Jain <ikritijain@gmail.com>
2019-04-04 15:48:43 +0530

    added some more functions

--- scripts/functions/length.js
+++ scripts/functions/length.js
@@ -11,18 +11,124 @@ function cmToM(cm) {
 function cmToMm(cm) {
 	return cm * 10;
 }
+
 function cmToKm(cm) {
 	return cm / 100000;
 }
+
 function cmToFeet(cm) {
 	return cm / 30.48;
 }
+
 function cmToYards(cm) {
 	return cm / 91.44;
 }
+
 function cmToMiles(cm) {
 	return cm / 160934.4;
 }
+
 function cmToNauticalMiles(cm) {
 	return cm / 185200;
 }
+
+// functions to convert mm into something
+function mmToCm(mm) {
+	return mm / 10;
+}
+
+function mmToM(mm) {
+	return mm / 1000;
+}
+
+function mmToKm(mm) {
+	return mm / 1000000;
+}
+
+function mmToInch(mm) {
+	return mm / 25.4;
+}
+
+function mmToFeet(mm) {
+	return mm / 304.8;
+}
+
+function mmToYards(mm) {
+	return mm / 914.4;
+}
+
+function mmToMiles(mm) {
+	return mm / 6.213711922e-7; //for an approximate answer
+}
+
+function mmToNauticalMiles(mm) {
+	return mm / 1.852e+6;
+}
+
+// functions to convert meter into something
+function mToMm(meter) {
+	return meter * 1000;
+}
+
+function mToCm(meter) {
+	return meter * 100;
+}
+
+function mToKm(meter) {
+	return meter / 1000;
+}
+
+function mToInch(meter) {
+	return meter * 39.37;
+}
+
+function mTofeet(meter) {
+	return meter * 3.281; //for an approximate answer
+}
+
+function mToYards(meter) {
+	return meter * 1.094; //for an approximate answer
+}
+
+function mToMiles(meter) {
+	return meter / 1609.344;
+}
+
+function mToNauticalMiles(meter) {
+	return meter / 1852;
+}
+
+// functions to convert kilometer into something
+function kmToMm(km) {
+	return km * 1000000;
+}
+
+function kmToCm(km) {
+	return km * 100000;
+}
+
+function kmToM(km) {
+	return km * 1000;
+}
+
+function kmToInch(km) {
+	return km * 39370.070; //for an approximate answer
+}
+
+function kmTofeet(km) {
+	return km * 3280.84;
+}
+
+function kmToYards(km) {
+	return km * 1093.613;
+}
+
+function kmToMiles(km) {
+	return km / 1.609;
+}
+
+function kmToNauticalmiles(km) {
+	return km / 1.852;
+}
+
+// functions to convert inch into something
