d8c2a3437c5ca9eb7be76af4a758e36a44ccd6af
Kriti Jain <ikritijain@gmail.com>
2019-04-17 16:59:15 +0530

    added area functions

--- scripts/functions/area.js
+++ scripts/functions/area.js
@@ -1,4 +1,135 @@
-// hello this is test 
-function cmTomm() {
-		// this is test kanika gupta
+// functions to convert cm into something
+
+function cm2toM2(cm) {
+	return cm / 0.0001;
+}
+
+function cm2toHa(cm) {
+	return cm / 1e+8;
+}	
+
+function cm2toKm2(cm) {
+	return cm / 1e+10;
+}
+
+function cm2toIn2(cm) {
+	return cm / 6.452;
+}
+
+function cm2toFt2(cm) {
+	return cm /  929.03;
+}
+
+function cm2toYd2(cm) {
+	return cm / 8361.274;
+}
+
+function cm2toMi2(cm) {
+	return cm / 2.59e+10;
+}
+
+function cm2toAc(cm) {
+	return cm /  4.047e+7;
+}
+
+// functions to convert cm into something
+
+function m2toCm2(m2) {
+	return m2 * 10000;
+}
+
+function m2toHa(m2) {
+	return m2 / 10000;
+}	
+
+function m2toKm2(m2) {
+	return m2 / 1e+6;
+}
+
+function m2toIn2(m2) {
+	return m2 * 1550.003;
+}
+
+function m2toFt2(m2) {
+	return m2 * 10.764;
+}
+
+function m2toYd2(m2) {
+	return m2 * 1.196;
+}
+
+function m2toMi2(m2) {
+	return m2 / 2.59e+6	;
+}
+
+function m2toAc(m2) {
+	return m2 /  4046.856;
+}
+
+// functions to convert cm into something
+
+function km2toCm2(km2) {
+	return km2 * 1e+10;
+}
+
+function km2toHa(km2) {
+	return km2 * 100;
+}	
+
+function km2tom2(km2) {
+	return km2 *  1e+6;
+}
+
+function km2toIn2(km2) {
+	return km2 * 1.55e+9;
+}
+
+function km2toFt2(km2) {
+	return km2 * 1.076e+7;
+}
+
+function km2toYd2(km2) {
+	return km2 * 1.196e+6;
+}
+
+function km2toMi2(km2) {
+	return km2 / 2.59;
+}
+
+function km2toAc(km2) {
+	return km2 * 247.105;
+}
+
+// functions to convert cm into something
+
+function hatoCm2(hac) {
+	return hac * 1e+8;
+}
+
+function hatoKm2(hac) {
+	return hac / 100;
+}	
+
+function hatoM2(hac) {
+	return hac /  1000;
+}
+
+function hatoIn2(hac) {
+	return hac * 1.55e+7;
+}
+
+function hatoFt2(hac) {
+	return hac * 107639.104;
+}
+
+function hatoYd2(hac) {
+	return hac * 11959.9;
+}
+
+function hatoMi2(hac) {
+	return hac / 258.999;
+}
+
+function hatoAc(hac) {
+	return hac * 2.471;
 }
