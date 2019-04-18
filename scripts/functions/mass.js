1729346a0b04c30b6d4084dfb64049eb0e1404fa
Kanika Gupta <kanika191.g@gmail.com>
2019-04-19 00:50:11 +0530

    completed mass functions

--- scripts/functions/mass.js
+++ scripts/functions/mass.js
@@ -1,1 +1,240 @@
-ffunction jsdfkjdk
+// functions to convert tonne into other units
+
+function tToKg(t) {
+	return t * 1000;
+}
+
+function tToGm(t) {
+	return t * 1e+6;
+}	
+
+function tToMg(t) {
+	return t * 1e+9;
+}
+
+function tToMicrog(t) {
+	return t * 1e+12;
+}
+
+function tToQ(t) {
+	return t * 10;
+}
+
+function tToLb(t) {
+	return t * 2204.623;
+}
+
+function tToOz(t) {
+	return t * 35273.962;
+}
+
+// functions to convert kilogram into other units
+
+function KgToT(kg) {
+	return kg / 1000;
+}
+
+function KgToGm(kg) {
+	return kg * 1000;
+}
+
+function KgToMg(kg) {
+	return kg * 1e+6;
+}
+
+function KgToMicrog(kg) {
+	return kg * 1e+9;
+}
+
+function KgToQ(kg) {
+	return kg / 100;
+}
+
+function KgToLb(kg) {
+	return kg * 2.205;
+}
+
+function KgToOz(kg) {
+	return kg * 35.274;
+}
+
+// functions to convert gram into other units
+
+function GmToT(gm) {
+	return gm / 1e+6 ;
+}
+
+function GmToKg(gm) {
+	return gm /1000 ;
+}
+
+function GmToMg(gm) {
+	return gm * 1000 ;
+}
+
+function GmToMicrog(gm) {
+	return gm * 1e+6 ;
+}
+
+function GmToQ(gm) {
+	return gm / 100000 ;
+}
+
+function GmToLb(gm) {
+	return gm / 453.592 ;
+}
+
+function GmToOz(gm) {
+	return gm / 28.35 ;
+}
+
+// functions to convert miligram into other units
+
+function MgToT(mg) {
+	return mg / 1e+9;
+}
+
+function MgToKg(mg) {
+	return mg / 1e+6;
+}
+
+function MgToGm(mg) {
+	return mg / 1000;
+}
+
+function MgToMicrog(mg) {
+	return mg * 1000;
+}
+
+function MgToQ(mg) {
+	return mg / 1e+8;
+}
+
+function MgToLb(mg) {
+	return mg / 453592.37;
+}
+
+function MgToOz(mg) {
+	return mg / 28349.523;
+}
+
+// functions to convert microgram into other units
+
+function MicrogToT(microg) {
+	return microg / 1e+12;
+}
+
+function MicrogToKg(microg) {
+	return microg / 1e+9;
+}
+
+function MicrogToGm(microg) {
+	return microg / 1e+6;
+}
+
+function MicrogToMg(microg) {
+	return microg / 1000;
+}
+
+function MicrogToQ(microg) {
+	return microg / 1e+11;
+}
+
+function MicrogToLb(microg) {
+	return microg / 4.536e+8;
+}
+
+function MicrogToOz(microg) {
+	return microg / 2.835e+7 ;
+}
+
+// functions to convert quintal to other units
+
+function QToT(quintal) {
+	return quintal / 10;
+}
+
+function QToKg(quintal) {
+	return quintal * 100;
+}
+
+function QToGm(quintal) {
+	return quintal * 100000;
+}
+
+function QToMg(quintal) {
+	return quintal * 1e+8;
+}
+
+function QToMicrog(quintal) {
+	return quintal * 1e+11;
+}
+
+function QToLb(quintal) {
+	return quintal * 220.462;
+}
+
+function QToOz(quintal) {
+	return quintal * 3527.396;
+}
+
+//functions to convert Pounds to other units
+
+function LbToT(pounds) {
+	return pounds / 2204.623;
+}
+
+function LbToKg(pounds) {
+	return pounds * 2.205;
+}
+
+function LbToGm(pounds) {
+	return pounds * 453.592;
+}
+
+function LbToMg(pounds) {
+	return pounds * 453592.37;
+}
+
+function LbToMicrog(pounds) {
+	return pounds * 4.536e+8;
+}
+
+function LbToQ(pounds) {
+	return pounds / 220.462;
+}
+
+function LbToOz(pounds) {
+	return pounds * 16;
+}
+
+//functions to convert ounce to other units
+
+function OzToT(ounce) {
+	return ounce / 35273.962;
+}
+
+function OzToKg(ounce) {
+	return ounce / 35.274;
+}
+
+function OzToGm(ounce) {
+	return ounce * 28.35;
+}
+
+function OzToMg(ounce) {
+	return ounce * 28349.523;
+}
+
+function OzToMicrog(ounce) {
+	return ounce * 2.835e+7;
+}
+
+function OzToQ(ounce) {
+	return ounce / 3527.396;
+}
+
+function OzToLb(ounce) {   //to convert ounce into pound
+	return ounce / 16;
+}
+
