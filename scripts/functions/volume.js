9ca7a9af7a11147906f0192af10bd598ef90cd56
Kanika Gupta <kanika191.g@gmail.com>
2019-04-19 17:00:58 +0530

    completed volume functions

---
+++ scripts/functions/volume.js
@@ -0,0 +1,242 @@
+// functions to convert mililitre into other units
+function MlToLtr(ml) {
+	return ml / 1000;
+}
+
+function MlToCubicM(ml) {
+	return ml / 1e+6 ;
+}
+
+function MlToCubicFt(ml) {
+	return ml / 28316.847;
+}
+
+function MlToCubicInch(ml) {
+	return ml / 16.387;
+}
+
+function MlToPint(ml) {
+	return ml / 568.261;
+}
+
+function MlToQuart(ml) {
+	return ml / 1136.522;
+}
+
+function MlToGallon(ml) {
+	return ml / 4546.09;
+}
+
+//functions to convert litre into other units
+
+function LtrToMl(ltr) {
+	return ltr * 1000;
+}
+
+function LtrToCubicM(ltr) {
+	return ltr * 1000;
+}
+
+function LtrToCubicFt(ltr) {
+	return ltr / 28.317;
+}
+
+function LtrToCubicInch(ltr) {
+	return ltr * 61.024;
+}
+
+function LtrToPint(ltr) {
+	return ltr * 1.76 ;
+}
+
+function LtrToQuart(ltr) {
+	return ltr / 1.137 ;
+}
+
+function LtrToGallon(ltr) {
+	return ltr / 4.546;
+}
+
+//functions to convert cubic meter into other units
+
+function CubicMToMl(cubicmtr) {
+	return cubicmtr * 1e+6 ;
+}
+
+function CubicMToLtr(cubicmtr) {
+	return cubicmtr * 1000;
+}
+
+function CubicMToCubicFt(cubicmtr) {
+	return cubicmtr * 35.315 ;
+}
+
+function CubicMToCubicInch(cubicmtr) {
+	return cubicmtr * 61023.744 ;
+}
+
+function CubicMToPint(cubicmtr) {
+	return cubicmtr * 1759.754;
+}
+
+function CubicMToQuart(cubicmtr) {
+	return cubicmtr * 879.877 ;
+}
+
+function CubicMToGallon(cubicmtr) {
+	return cubicmtr * 219.969 ;
+}
+
+// functions to convert Cubic feet into other units
+
+function CubicFtToMl(cubicft) {
+	return cubicft * 28316.847 ;
+}
+
+function CubicFtToLtr(cubicft) {
+	return cubicft * 28.317;
+}
+
+function CubicFtToCubicM(cubicft) {
+	return cubicft * 35.315 ;
+}
+
+function CubicFtToCubicInch(cubicft) {
+	return cubicft * 1728 ;
+}
+
+function CubicFtToPint(cubicft) {
+	return cubicft * 49.831 ;
+}
+
+function CubicFtToQuart(cubicft) {
+	return cubicft * 24.915;
+}
+
+function CubicFtToGallon(cubicft) {
+	return cubicft * 6.229 ;
+}
+
+
+//functions to convert cubicinch to other units
+
+function CubicInchToMl(cubicinch) {
+	return cubicinch * 16.387 ;
+}
+
+function CubicInchToLtr(cubicinch) {
+	return cubicinch / 61.024 ;
+}
+
+function CubicInchToCubicM(cubicinch) {
+	return cubicinch / 61023.744 ;
+}
+
+function CubicInchToCubicFt(cubicinch) {
+	return cubicinch / 1728 ;
+}
+
+function CubicInchToPint(cubicinch) {
+	return cubicinch / 34.677 ;
+}
+
+function CubicInchToQuart(cubicinch) {
+	return cubicinch / 69.335 ;
+}
+
+function CubicInchToGallon(cubicinch) {
+	return cubicinch / 277.419 ;
+}
+
+//functions to convert pint to other units
+
+function PintToMl(pint) {
+	return pint * 568.261;
+}
+
+function PintToLtr(pint) {
+	return pint / 1.76;
+}
+
+function PintToCubicM(pint) {
+	return pint / 1759.754;
+}
+
+function PintToCubicFt(pint) {
+	return pint / 49.831 ;
+}
+
+function PintToCubicInch(pint) {
+	return pint * 34.677 ;
+}
+
+function PintToQuart(pint) {
+	return pint / 2;
+}
+
+function PintToGallon(pint) {
+	return pint / 8 ;
+}
+
+//functions to convert quart to other units
+
+function QuartToMl(quart) {
+	return quart * 1136.522;
+}
+
+function QuartToLtr(quart) {
+	return quart * 1.137;
+}
+
+function QuartToCubicM(quart) {
+	return quart / 879.877;
+}
+
+function QuartToCubicInch(quart) {
+	return quart * 69.335 ;
+}
+
+function QuartToCubicFt(quart) {
+	return quart / 24.915 ;
+}
+
+function QuartToPint(quart) {
+	return quart * 2 ;
+}
+
+function QuartToGallon(quart) {
+	return quart / 4 ;
+}
+
+//functions to convert gallon to other units
+
+function GallonToMl(gallon) {
+	return gallon  * 4546.09 ;
+}
+
+function GallonToLtr(gallon) {
+	return gallon  * 4.546 ;
+}
+
+function GallonToCubicM(gallon) {
+	return gallon  / 219.969 ;
+}
+
+function GallonToCubicInch(gallon) {
+	return gallon  * 277.419 ;
+}
+
+function GallonToCubicFt(gallon) {
+	return gallon  / 6.229 ;
+}
+
+function GallonToPint(gallon) {
+	return gallon  * 8 ;
+}
+
+function GallonToQuart(gallon) {
+	return gallon  * 4 ;
+}
+
+
+
