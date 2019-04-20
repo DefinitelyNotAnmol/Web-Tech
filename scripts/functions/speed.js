7a26a12692e90a4d676316d2df092c6eaa4fc944
Anmol Jain <itsmeaj20@gmail.com>
2019-04-19 17:30:19 -0700

    updated speed.j (added si units)

--- scripts/functions/speed.js
+++ scripts/functions/speed.js
@@ -1,181 +1,182 @@
-// functions to convert meter per second into something
+// slight - Speed of Light
+// ssoung - Speed of Sound
+// kn - Knot
+// fts - foot per second
+// mph - miles per hour
+// kmph - Kilometer per hour
+// ms - meter per second
 
-function mstokmh(ms) {
+// functions to convert meter per second into something
+function msToKmph(ms) {
 	return ms * 3.6;
 }
 
-function mstomph(ms) {
+function msToMph(ms) {
 	return ms * 2.237;
 }
 
-function mstofts(ms) {
+function msToFts(ms) {
 	return ms * 3.281;
 }
 
-function mstokn(ms) {
+function msToKn(ms) {
 	return ms * 1.944;
 }
 
-function mstoSsound(ms) {
+function msToSsound(ms) {
 	return ms / 343;
 }
 
-function mstoSlight(ms) {
+function msToSlight(ms) {
 	return ms /  2.998e+8;
 }
 
 // functions to convert kilometer per hour into something
-
-function kmhtoMs(kmh) {
-	return kmh / 3.6;
+function kmphToMs(kmph) {
+	return kmph / 3.6;
 }
 
-function kmhtoMph(kmh) {
-	return kmh / 1.609;
+function kmphToMph(kmph) {
+	return kmph / 1.609;
 }
 
-function kmhtoFts(kmh) {
-	return kmh / 1.097;
+function kmphToFts(kmph) {
+	return kmph / 1.097;
 }
 
-function kmhtoKn(kmh) {
-	return kmh / 1.852;
+function kmphToKn(kmph) {
+	return kmph / 1.852;
 }
 
-function kmhtoSsound(kmh) {
-	return kmh /  1234.8;
+function kmphToSsound(kmph) {
+	return kmph /  1234.8;
 }
 
-function kmhtoSlight(kmh) {
-	return kmh /  1.079e+9;
+function kmphToSlight(kmph) {
+	return kmph /  1.079e+9;
 }
 
 // functions to convert miles per hour into something
-
-function mphtoMs(mph) {
+function mphToMs(mph) {
 	return mph /  2.237;
 }
 
-function mphtoKmh(mph) {
+function mphToKmph(mph) {
 	return mph * 1.609;
 }
 
-function mphtoFts(mph) {
+function mphToFts(mph) {
 	return mph * 1.467;
 }
 
-function mphtoKn(mph) {
+function mphToKn(mph) {
 	return mph / 1.151;
 }
 
-function mphtoSsound(mph) {
+function mphToSsound(mph) {
 	return mph /  767.269;
 }
 
-function mphtoSlight(mph) {
+function mphToSlight(mph) {
 	return mph / 6.706e+8;
 }
 
 // functions to convert foot per second into something
-
-function ftstoMs(fts) {
+function ftsToMs(fts) {
 	return fts /  3.281;
 }
 
-function ftstoKmh(fts) {
+function ftsToKmph(fts) {
 	return fts * 1.097;
 }
 
-function ftstoMph(fts) {
+function ftsToMph(fts) {
 	return fts / 1.467;
 }
 
-function ftstoKn(fts) {
+function ftsToKn(fts) {
 	return fts / 1.688;
 }
 
-function ftstoSsound(fts) {
+function ftsToSsound(fts) {
 	return fts /  1125.328;
 }
 
-function ftstoSlight(fts) {
+function ftsToSlight(fts) {
 	return fts / 9.836e+8;
 }
 
 // functions to convert knot into something
-
-function kntoMs(kn) {
+function knToMs(kn) {
 	return kn /  1.944;
 }
 
-function kntoKmh(kn) {
+function knToKmph(kn) {
 	return kn * 1.852;
 }
 
-function kntoMph(kn) {
+function knToMph(kn) {
 	return kn * 1.151;
 }
 
-function kntoFts(kn) {
+function knToFts(kn) {
 	return kn * 1.688;
 }
 
-function kntoSsound(kn) {
+function knToSsound(kn) {
 	return kn / 666.739;
 }
 
-function kntoSlight(kn) {
+function knToSlight(kn) {
 	return kn / 5.827e+8;
 }
 
 // functions to convert speed of sound into something
-
-function ssoundtoMs(ssound) {
+function ssoundToMs(ssound) {
 	return ssound * 343;
 }
 
-function ssoundtoKmh(ssound) {
+function ssoundToKmph(ssound) {
 	return ssound * 1234.8;
 }
 
-function ssoundtoMph(ssound) {
+function ssoundToMph(ssound) {
 	return ssound * 767.269;
 }
 
-function ssoundtoFts(ssound) {
+function ssoundToFts(ssound) {
 	return ssound * 1125.328;
 }
 
-function ssoundtoKn(ssound) {
+function ssoundToKn(ssound) {
 	return ssound * 666.739;
 }
 
-function ssoundtoSlight(ssound) {
+function ssoundToSlight(ssound) {
 	return ssound / 874030.49;
 }
 
 // functions to convert speed of light into something
-
-function slighttoMs(slight) {
+function slightToMs(slight) {
 	return slight * 2.998e+8;
 }
 
-function slighttoKmh(slight) {
+function slightToKmph(slight) {
 	return slight * 1.079e+9;
 }
 
-function slighttoMph(slight) {
+function slightToMph(slight) {
 	return slight * 6.706e+8;
 }
 
-function slighttoFts(slight) {
+function slightToFts(slight) {
 	return slight * 9.836e+8;
 }
 
-function slighttoKn(slight) {
+function slightToKn(slight) {
 	return slight * 5.827e+8;
 }
 
-function slighttoSsound(slight) {
+function slightToSsound(slight) {
 	return slight * 874030.49;
 }
