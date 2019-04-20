b3d879103b82563bb7b0db86ee084bd16a24b808
Anmol Jain <itsmeaj20@gmail.com>
2019-04-19 17:33:38 -0700

    updated temperature.js (added si units)

--- scripts/functions/temperature.js
+++ scripts/functions/temperature.js
@@ -1,29 +1,29 @@
 // Celsius to Fahrenheit conversion
-function CtoF(c) {
+function cToF(c) {
 	return (c * 9/5) + 32;
 }	
 
 // Celsius to kelvin conversion
-function CtoK(c) {
+function cToK(c) {
 	return c + 273.15;
 }	
 
 // fahrenheit to celsius conversion
-function FtoC(f,c) {
+function fToC(f,c) {
 	return c = (f - 32) * 5 / 9;
 }	
 
 // fahrenheit to kelvin conversion
-function FtoK(f) {
+function fToK(f) {
 	return 273.5 + ((f - 32.0) * (5.0/9.0)); 
 }	
 
 // kelvin to celcius conversion
-function KtoC(k) {
+function kToC(k) {
 	return k-273.15;
 }	
 
 // kelvin to fahrenheit conversion
-function KtoF(k) {
+function kToF(k) {
   return (k-273)*9/5+32;
 }
