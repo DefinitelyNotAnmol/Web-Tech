978bc6d2c578f157c840e2a3db6a6ca6fd98903c
Anmol Jain <itsmeaj20@gmail.com>
2019-04-20 03:04:04 -0700

    Linking Stage 3 [js files]

---
+++ scripts/area.js
@@ -0,0 +1,6 @@
+function calculate() {
+    var x = document.getElementById("menu1").value;
+    var y = document.getElementById("menu2").value;
+    document.getElementById("result").innerHTML 
+        = document.getElementById("input").value;
+}
